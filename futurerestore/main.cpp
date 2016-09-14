f06be0c33976b6a31c90b8c43b792848256e690f
tihmstar <tihmstar@gmail.com>
2016-09-14 20:35:09 +0200

    Initial Commit

---
+++ futurerestore/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  futurerestore
+//
+//  Created by tihmstar on 14.09.16.
+//  Copyright © 2016 tihmstar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
