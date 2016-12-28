2c0aaf0b823511146717f5fa1169cad68262b1aa
Nikias Bassen <nikias@gmx.li>
2016-12-28 17:43:26 +0100

    Use autotools to build project

---
+++ autogen.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+gprefix=`which glibtoolize 2>&1 >/dev/null`
+if [ $? -eq 0 ]; then 
+  glibtoolize --force
+else
+  libtoolize --force
+fi
+aclocal -I m4
+autoheader
+automake --add-missing
+autoconf
+
+export NOCONFIGURE=1
+
+SUBDIRS="external/idevicerestore external/img4tool external/tsschecker"
+for SUB in $SUBDIRS; do
+    pushd $SUB
+    ./autogen.sh
+    popd
+done
+
+unset NOCONFIGURE
+
+if [ -z "$NOCONFIGURE" ]; then
+    ./configure "$@"
+fi
