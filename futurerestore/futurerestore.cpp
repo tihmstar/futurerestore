a1d8cfaf952f10907cef3523d1f985015bffab7c
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2021-07-21 00:37:19 -0400

    An unplugged cable is not 32-bit (#24)
    * An unplugged cable is not 32-bit
    
    * Better practice
    
    * Forgot extra parentheses

--- futurerestore/futurerestore.cpp
+++ futurerestore/futurerestore.cpp
@@ -119,11 +119,13 @@ futurerestore::futurerestore(bool isUpdateInstall, bool isPwnDfu, bool noIBSS) :
 
 bool futurerestore::init(){
     if (_didInit) return _didInit;
-    _didInit = (check_mode(_client) != MODE_UNKNOWN);
-    if (!(_client->image4supported = is_image4_supported(_client))){
-        info("[INFO] 32-bit device detected\n");
-    }else{
-        info("[INFO] 64-bit device detected\n");
+//    If device is in an invalid state, don't check if it supports img4
+    if ((_didInit = check_mode(_client) != MODE_UNKNOWN)) {
+        if (!(_client->image4supported = is_image4_supported(_client))){
+            info("[INFO] 32-bit device detected\n");
+        }else{
+            info("[INFO] 64-bit device detected\n");
+        }
     }
     return _didInit;
 }
