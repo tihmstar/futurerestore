b7710d47d55551f1f71b117fd46b15861260bcff
Cryptiiiic <liamwqs@gmail.com>
2022-10-09 00:16:17 -0700

    Fix actions

--- .github/workflows/mac-post.sh
+++ .github/workflows/mac-post.sh
@@ -12,13 +12,13 @@ export FUTURERESTORE_VERSION_RELEASE=$(cat version.txt | tr -d '\n')
 echo -n $FUTURERESTORE_VERSION_SHA > latest_build_sha.txt
 echo -n $FUTURERESTORE_VERSION > latest_build_num.txt
 lipo -create -arch x86_64 cmake-build-release-x86_64/src/futurerestore -arch arm64 cmake-build-release-arm64/src/futurerestore -output futurerestore
-codesign --deep --force --sign - futurerestore
+#codesign --force --sign - futurerestore
 tar cpPJf "futurerestore-macOS-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-RELEASE.tar.xz" futurerestore
 rm -rf futurerestore
 lipo -create -arch x86_64 cmake-build-debug-x86_64/src/futurerestore -arch arm64 cmake-build-debug-arm64/src/futurerestore -output futurerestore
-codesign --deep --force --sign - futurerestore
+#codesign --force --sign - futurerestore
 tar cpPJf "futurerestore-macOS-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-DEBUG.tar.xz" futurerestore
 rm -rf futurerestore
 lipo -create -arch x86_64 cmake-build-asan-x86_64/src/futurerestore -arch arm64 cmake-build-asan-arm64/src/futurerestore -output futurerestore
-codesign --deep --force --sign - futurerestore
+#codesign --force --sign - futurerestore
 tar cpPJf "futurerestore-macOS-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-ASAN.tar.xz" futurerestore
